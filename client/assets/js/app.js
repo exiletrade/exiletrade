176b5e9f5b4d8db572228c9de53cd3434c98b44a
thirdy <thirdy.derivera@gmail.com>
2016-03-09 09:48:19 +0800

    fixed cases where we have empty string

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -60,6 +60,7 @@ function parseSearchInputTokens(input) {
 	var badTokens = [];
 	for (i in tokens) {
 		var evaluatedToken = tokens[i];
+		if (!evaluatedToken) continue;
 		var token = evaluatedToken.toUpperCase();
 		
 		if (/^(OR|AND|LST|NOT)$/i.test(token)) {
